0a1937b867cd905e0e8f5d19c3c70c41a008d1b7
Mahmoudi Azita <am222hdm-stuttgert.de>
2021-06-04 20:56:01 +0200

    feat: Add ArrayData

--- SoftwareProjekt/src/main/java/org/example/App.java
+++ SoftwareProjekt/src/main/java/org/example/App.java
@@ -11,39 +11,16 @@ public class App {
 
   public static void main(String[] args) throws IOException {
 
-    //path to the .txt file
-    FileReader fr = new FileReader("C:\\Users\\dango\\IdeaProjects\\software-entwicklung\\SoftwareProjekt\\src\\main\\java\\org\\example\\SDR.txt");
-    //reading only the first line
-    BufferedReader br = new BufferedReader(fr);
+
     //Scanning the input from the user
     Scanner scanner = new Scanner(System.in);
     //Eingabe.java class
     Eingabe eingabe = new Eingabe();
+    ArrayData ray = new ArrayData();
 
-    //Arrays
-    String[] currency = new String[39];
-    String[] wert = new String[39];
+    ray.fillingArray();
+    ray.giveString();
 
-    //Variables for the arrays
-    int i = 0;
-    int a = 0;
-    int x = 0;
-    int z = 0;
-
-    //filling the arrays with currency names and SDR-Value
-    while (i < 78) {
-      if (a % 2 == 0) {
-        currency[x] = br.readLine();
-        System.out.println(currency[x]);
-        x++;
-      } else {
-        wert[z] = br.readLine();
-        System.out.println(wert[z]);
-        z++;
-      }
-      a++;
-      i++;
-    }
 
 
     //Interface and InputMismatchException catch
@@ -54,6 +31,7 @@ public class App {
     }
 
 
+    scanner.close();
   }
 
 
